b005bf0a194ca5d12993c2b1cca4202ea7889df7
NitenSapkota175 <nitensapkota1705@gmail.com>
2023-09-16 12:21:54 +0530

    lsiting form created and validation are added

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,14 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Models\Listing;
 use Symfony\Component\CssSelector\Node\FunctionNode;
+// All Listings
 Route::get('/', [ListingController::class , 'index']);
 
+// Show Cretae Form
+Route::get('/listings/create',[ListingController::class , 'create']);
+
+// Store Listing Data
+Route::post('/listings',[ListingController::class , 'store']);
+
 // single listing using route model binding
-Route::get('listings/{listing}',[ListingController::class , 'show']);
+Route::get('/listings/{listing}',[ListingController::class , 'show']);
