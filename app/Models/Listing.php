d82d934876627d57724fda39edef2d5f0330a83c
NitenSapkota175 <nitensapkota1705@gmail.com>
2023-09-17 00:09:19 +0530

    relationship between listings and users added

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -23,4 +23,9 @@ class Listing extends Model
             ->orWhere('description','like','%'.request('search').'%');
         }
     }
+
+    // Relationship To User
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
